d5a213b965ced3ef55888f8b8fd18224f58d6f2b
vancronew <58564813+vancronew@users.noreply.github.com>
2019-12-11 17:00:10 -0500

    Update request.js

--- js/request.js
+++ js/request.js
@@ -1,26 +1,26 @@
 function validateRequestForm() {
     let isFormValid = true;
-    let newemail = $("#mce-EMAIL_ADDRESS").val();
+    let newemail = $("#mce-Email_Address").val();
     clearRequestFormErrors();
-    if ($("#mce-COMPANY_NAME").val().length < 1) {
+    if ($("#mce-Company_Name").val().length < 1) {
         isFormValid = false;
         $("#companyError").show();
-    }else if ($("#mce-CONTACT_NAME").val().length < 1) {
+    }else if ($("#mce-Contact_Name").val().length < 1) {
         isFormValid = false;
         $("#contactError").show();
     } else if (newemail.length < 1 || !isValidEmail(newemail)) {
         isFormValid = false;
         $("#newEmailError").show();
-    } else if ($("#mce-PHONE_NUMBER").val().length < 1) {
+    } else if ($("#mce-Phone_Number").val().length < 1) {
         isFormValid = false;
         $("#phoneError").show();
-    }else if ($("#mce-TIME_OF_SERVICES_REQUESTED").val().length < 1) {
+    }else if ($("#mce-Time_Of_Services_Requested").val().length < 1) {
         isFormValid = false;
         $("#timeError").show();
-    }else if ($("#mce-LOCATION_OF_SERVICES_REQUESTED").val().length < 1) {
+    }else if ($("#mce-Location_Of_Services_Requested").val().length < 1) {
         isFormValid = false;
         $("#locationError").show();
-    }else if ($("#mce-NATURE_OF_REQUEST").val().length < 1) {
+    }else if ($("#mce-Nature_Of_Request").val().length < 1) {
         isFormValid = false;
         $("#natureError").show();
     }
